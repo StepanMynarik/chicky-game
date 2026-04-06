8bb41c8f14a7ca4ab350774a812f421cfc7e65eb
Štěpán Mynařík <46678816+StepanMynarik@users.noreply.github.com>
2026-04-06 09:59:17 +0200

    Update ad-block.user.js

--- ad-block.user.js
+++ ad-block.user.js
@@ -1,29 +1,12 @@
 // ==UserScript==
 // @name         Ad-Block
 // @match        https://calendar.google.com/*
-// @run-at       document-start
+// @include      https://calendar.google.com/*
+// @run-at       document-end
 // ==/UserScript==
 
 (function () {
-  const registry = new WeakMap();
-  const orig = EventTarget.prototype.addEventListener;
-
-  EventTarget.prototype.addEventListener = function (type, listener, options) {
-    if (!registry.has(this)) registry.set(this, []);
-    registry.get(this).push({ type, listener, options });
-    return orig.call(this, type, listener, options);
-  };
-
-  window.__removeHandlers = (selector, eventType) => {
-    document.querySelectorAll(selector).forEach(el => {
-      const listeners = registry.get(el) || [];
-      listeners
-        .filter(l => l.type === eventType)
-        .forEach(({ type, listener, options }) => {
-          el.removeEventListener(type, listener, options);
-        });
-    });
-  };
-  
-  setInterval(() => window.__removeHandlers('[role=presentation]', 'click'), 500);
+  const style = document.createElement('style');
+  style.textContent = '[role=presentation], [role=presentation] * { pointer-events: none !important; }';
+  document.head.appendChild(style);
 })();
